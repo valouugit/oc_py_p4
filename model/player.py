d0b4612c84596b9aa66160496e3b2180530f7eae
valouugit <valentingoulier@gmail.com>
2022-06-11 11:11:20 +0200

    Add gender & change datetime for tournament

--- model/player.py
+++ model/player.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 
+from model.gender import Gender
+
+
 @dataclass
 class Player:
-    first_name : str
-    last_name : str
-    birthday : str
-    sexe : str
-    position : int
+    first_name: str
+    last_name: str
+    birthday: str
+    gender: Gender
+    position: int
--- model/tournament.py
+++ model/tournament.py
@@ -1,16 +1,19 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from datetime import datetime
+
+from typing import List
 from .round import Round
 from .player import Player
 from .timer import Timer
 
+
 @dataclass
 class Tournament:
-    name : str
-    location : str
-    date : str
-    rounds : List[Round]
-    players : List[Player]
-    timer : Timer
-    description : Optional[str] = None
-    rounds_number : int = 4
+    name: str
+    location: str
+    date: datetime
+    timer: Timer
+    rounds: List[Round]
+    players: List[Player]
+    description: str | None = None
+    rounds_number: int = 4
